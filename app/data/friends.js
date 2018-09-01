e52f3d8648675f48f8f15626d3f1553aebf7f758
Stephen Pellegrini <spellegrini156@gmail.com>
2018-09-01 14:10:30 -0700

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+//imaginary profiles + placeholder images
+var friends = [{
+	name: "Abby",
+		scores: [2, 2, 3, 1, 5, 2, 5, 3, 5, 4],
+	photo: "https://cdn.pixabay.com/photo/2018/07/29/10/35/animals-3569825_960_720.jpg",
+}, {
+	name: "Frank",
+		scores: [1, 2, 3, 5, 2, 1, 5, 2, 3, 3],
+	photo: "https://cdn.pixabay.com/photo/2018/08/24/00/34/squirrel-3627114_960_720.jpg",
+
+}, {
+	name: "Burt",
+	scores: [2, 4, 3, 3, 1, 4, 5, 1, 3, 1],
+	photo: "https://cdn.pixabay.com/photo/2018/08/15/11/48/chicken-3607865_960_720.jpg",
+	
+}, {
+	name: "Sally",
+		scores: [2, 1, 1, 4, 2, 2, 2, 5, 2, 5],
+	photo: "https://cdn.pixabay.com/photo/2018/08/23/04/35/tabby-cat-3625227_960_720.jpg",
+
+}, {
+	name: "Gail",
+		scores: [2, 2, 5, 2, 4, 2, 5, 2, 3, 3],
+	photo: "https://cdn.pixabay.com/photo/2018/08/23/00/55/mold-3625017_960_720.jpg",
+
+}, {
+	name: "Hank",
+		scores: [1, 2, 3, 5, 2, 1, 5, 2, 3, 3],
+	photo: "https://cdn.pixabay.com/photo/2015/11/16/06/02/lemur-1045220_960_720.jpg",
+
+}, {
+	name: "Billy",
+		scores: [2, 4, 3, 3, 1, 4, 5, 1, 3, 1],
+	photo: "https://cdn.pixabay.com/photo/2018/07/23/18/12/giraffes-3557499_960_720.jpg",
+
+}, {
+	name: "Mo",
+	scores: [2, 1, 1, 4, 2, 2, 2, 5, 2, 5],
+	photo: "https://cdn.pixabay.com/photo/2018/08/03/22/45/orang-utan-3582883_960_720.jpg",
+
+}, {
+	name: "Will",
+	photo: "https://cdn.pixabay.com/photo/2018/08/14/22/40/gorilla-3606809_960_720.jpg",
+	scores: [2, 2, 5, 2, 4, 2, 5, 2, 3, 3]
+}];
+module.exports = friends;
